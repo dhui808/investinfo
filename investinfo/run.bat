44081699a2f2c0300c7ecb6cc3d75575db531552
DESKTOP-EV9OUQ1\huich <huichunwuca@yahoo.com>
2023-05-12 20:29:23 -0400

    jackson 2.14.0

--- investinfo/run.bat
+++ investinfo/run.bat
@@ -2,5 +2,5 @@ set LIB_PATH=target\lib
 set LIB=%LIB_PATH%\jna-5.6.0.jar;%LIB_PATH%\jna-platform-5.6.0.jar
 set LIBREOFFICE_PATH="C:\Program Files\LibreOffice"\program\classes
 set LIBREOFFICE_LIB=%LIBREOFFICE_PATH%\libreoffice.jar;%LIBREOFFICE_PATH%\unoloader.jar;%LIBREOFFICE_PATH%\officebean.jar
-set CP=target\classes;..\jloputility\target\classes;%LIB%;%LIBREOFFICE_LIB%;%LIB_PATH%\jsoup-1.14.2.jar;%LIB_PATH%\commons-io-2.7.jar;%LIB_PATH%\jackson-core-2.11.2.jar;%LIB_PATH%\jackson-databind-2.11.2.jar;%LIB_PATH%\jackson-annotations-2.11.2.jar;%LIB_PATH%\mysql-connector-java-8.0.21.jar;%LIB_PATH%\commons-lang3-3.11.jar
+set CP=target\classes;..\jloputility\target\classes;%LIB%;%LIBREOFFICE_LIB%;%LIB_PATH%\jsoup-1.14.2.jar;%LIB_PATH%\commons-io-2.7.jar;%LIB_PATH%\jackson-core-2.14.0.jar;%LIB_PATH%\jackson-databind-2.14.0.jar;%LIB_PATH%\jackson-annotations-2.14.0.jar;%LIB_PATH%\mysql-connector-java-8.0.21.jar;%LIB_PATH%\commons-lang3-3.11.jar
 "%JAVA_HOME%/bin/java" -classpath %CP% cftc.Main %1 %2 %3 %4 %5
