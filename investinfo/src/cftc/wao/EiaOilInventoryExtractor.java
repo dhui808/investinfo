395e7ba541fb2304fe67397ce0372e49aaff2ccc
DESKTOP-EV9OUQ1\huich <huichunwuca@yahoo.com>
2024-04-06 12:15:18 -0400

    fix headers

--- investinfo/src/cftc/wao/EiaOilInventoryExtractor.java
+++ investinfo/src/cftc/wao/EiaOilInventoryExtractor.java
@@ -1,6 +1,7 @@
 package cftc.wao;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,8 +17,18 @@ public class EiaOilInventoryExtractor {
 	 */
 	public static String[] retrieveOilInventory() throws IOException {
 		
+		HashMap<String, String> headers = new HashMap<String, String>();
+		headers.put("Connection", "keep-alive");
+		headers.put("Host", "www.eia.gov");
+		headers.put("Sec-Fetch-Dest", "document");
+		headers.put("Sec-Fetch-Mode", "navigate");
+		headers.put("Sec-Fetch-Site", "none");
+		headers.put("Sec-Fetch-User", "?1");
+		headers.put("Upgrade-Insecure-Requests", "1");
+		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
+		
 		String url = "https://www.eia.gov/dnav/pet/PET_SUM_SNDW_A_EPC0_SAX_MBBL_W.htm";
-		Document doc = Jsoup.connect(url).get();
+		Document doc = Jsoup.connect(url).headers(headers).get();
 		Elements dataRows = doc.getElementsByClass("DataRow");
 		Element currentDataRow = dataRows.first();
 		Element currentInv = currentDataRow.getElementsByClass("Current2").first();
